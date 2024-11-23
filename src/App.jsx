16b1443cfc787abd96ab757fdac444bfcc6e73a8
SeydaBeypazarli <seydabeypazarli@gmail.com>
2024-11-23 12:31:47 +0300

    sayfayı resetler

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import workintech from '/workintech.svg'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
